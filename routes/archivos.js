ded4ed3ea504eaa35000eb5124db49c1f411e8b0
Erickowski <daniel_perez_03081995@hotmail.com>
2020-11-06 12:57:46 -0600

    Some issues

--- routes/archivos.js
+++ routes/archivos.js
@@ -2,15 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { check } = require("express-validator");
 
-const {
-  subirArchivo,
-  eliminarArchivo,
-} = require("../controllers/archivosController");
+const { subirArchivo } = require("../controllers/archivosController");
 
 const auth = require("../middleware/auth");
 
 router.post("/", auth, subirArchivo);
-
-router.delete("/:id", eliminarArchivo);
 
 module.exports = router;
