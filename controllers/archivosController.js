824510ed7f5c9c34c99b247680729c62894499f6
Erickowski <daniel_perez_03081995@hotmail.com>
2020-11-06 11:41:09 -0600

    Subiendo archivos al servidor

--- controllers/archivosController.js
+++ controllers/archivosController.js
@@ -1,3 +1,31 @@
-exports.subirArchivo = (req, res) => {};
+const multer = require("multer");
+const shortid = require("shortid");
+
+const configuracionMulter = {
+  limits: { fileSize: 1000000 },
+  storage: (fileStorage = multer.diskStorage({
+    destination: (req, res, cb) => {
+      cb(null, __dirname + "/../uploads");
+    },
+    filename: (req, file, cb) => {
+      const extension = file.mimetype.split("/")[1];
+      cb(null, `${shortid.generate()}.${extension}`);
+    },
+  })),
+};
+
+const upload = multer(configuracionMulter).single("archivo");
+
+exports.subirArchivo = async (req, res, next) => {
+  upload(req, res, async (error) => {
+    console.log(req.file);
+    if (!error) {
+      res.json({ archivo: req.file.filename });
+    } else {
+      console.log(error);
+      return next();
+    }
+  });
+};
 
 exports.eliminarArchivo = (req, res) => {};
